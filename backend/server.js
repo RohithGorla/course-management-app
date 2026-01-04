a62b16353662717c6e788c33ac90fadd8861594c
RohithGorla <rvphanivarma@example.com>
2026-01-04 22:25:39 +0530

    Fix port for Render

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
